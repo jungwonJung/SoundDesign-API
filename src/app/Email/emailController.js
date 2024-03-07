c2965d79f7ab9d568d673332d934ffe3c56bbf34
unknown <wjdwjd1501@gmail.com>
2024-03-07 15:05:45 +0100

    changed url

--- src/app/Email/emailController.js
+++ src/app/Email/emailController.js
@@ -8,7 +8,7 @@ const emailController = {
           const { email } = req.query;
           await confirmEmail(email);
           console.log(await confirmEmail(email))
-          res.send('<script type="text/javascript">alert("Successfully verified"); window.location="https://sdesign-api-jayganzi.koyeb.app/"; </script>');
+          res.send('<script type="text/javascript">alert("Successfully verified"); window.location="https://sdesign-api-jayganzi.koyeb.app"; </script>');
         } catch (error) {
           console.log(error);
           res.status(500).send('Error confirming email');
